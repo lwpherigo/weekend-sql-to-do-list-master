a30617615fa71fc5bea106434eef847780c7943f
Lucas Pherigo <lucas.pherigo@gmail.com>
2019-12-01 23:37:55 -0600

    DELETE router is functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,14 +4,17 @@ $(document).ready(init);
 
 function init() {
     console.log('JQ is ON');
-    $('#js-addItem').on('click', getItem)
+    getList();
+    $('#js-addItem').on('click', getItem);
+    $('.js-list').on('click', '.js-btn-complete', completeBtn);
+    $('.js-list').on('click', '.js-btn-delete', deleteItem);
 }
 
 function getItem(event) {
     console.log('hey');
     const newItem = {
-        item: $('#js-newToDo').val(),
-    }
+        task: $('#js-newToDo').val(),
+    };
 
     postItem(newItem);
 }
@@ -45,17 +48,65 @@ function getList() {
     })
 }
 
+function completeBtn() {
+    let id = $(this).data('id');
+    let val = true;
+    let turnGreen = $(this).data('trans');
+
+    if (val === true) {
+        turnGreen = document.getElementById(id).style.background = 'green';
+    } else {
+        turnGreen = turnGreen;
+    }
+    console.log(turnGreen);
+    putTask(turnGreen, id);
+}
+
+function putTask(complete, id) {
+    console.log(complete);
+    $.ajax({
+        method: 'PUT',
+        url: '/api/list/' + id,
+        data: {
+            complete: complete
+        }
+    })
+    .then((response) => {
+        console.log('PUT LIST');
+        getList();
+    }) 
+    .catch((err) => {
+        console.warn(err);
+    })
+}
+
+function deleteItem() {
+    console.log($(this).data('id'));
+    const idNum = $(this).data('id');
+
+    $.ajax({
+        method: "DELETE",
+        url: '/api/list/' + idNum
+    })
+    .then((response) => {
+        getList();
+    })
+    .catch((response) => {
+        console.warn(response);
+    })
+};
+
 function render(response) {
     $('.js-list').empty();
 
     for(let i = 0; i < response.length; i++) {
-        const listItem = response[i];
-
+        const listItem = response[i]; 
         $('.js-list').append(`
-            <ul>
-                <li>${listItem.task}</li>
-            </ul>
-        `)        
+            <tr>
+            <td>${listItem.task}</td>
+            <td><button class='js-btn-complete' data-trans="${listItem.task}" data-id="${listItem.id}">Complete!</button></td>
+            <td><button class='js-btn-delete' data-trans="${listItem.task}" data-id="${listItem.id}">Delete!</button></td>
+            </tr>
+        `)
     }
-}
-
+};
--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool')
 
 // GET
 router.get('/', (req,res) => {
-    pool.query(`SELECT * "list" ORDER BY "id" ASC;`)
+    pool.query(`SELECT * FROM "list" ORDER BY "id" ASC;`)
     .then((response) => {
         res.send(response.rows);
         console.log('get sent from server')
@@ -19,7 +19,7 @@ router.post('/', (req,res) => {
     const newItem = req.body;
     console.log(newItem);
     const queryString = `INSERT INTO "list" ("task")
-                        VALUES ('${newItem.item}');`;
+                        VALUES ('${newItem.task}');`;
     
     pool.query(queryString)
     .then((response) => {
@@ -30,6 +30,36 @@ router.post('/', (req,res) => {
     })
 });
 
+// PUT
+router.put('/:id', (req,res) => {
+    const id = req.params.id;
+    const complete = req.body.complete;
+
+    let queryString = `UPDATE "list" SET "task"='${complete}' WHERE "id" = $1;`;
+    console.log(queryString);
+    pool.query(queryString, [id])
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+});
+
 // DELETE
+router.delete('/:id', (req,res) => {
+    console.log(req.params.id);
+    const taskId = req.params.id;
+    const queryString = `DELETE FROM "list" WHERE "id" = ${taskId};`;
+
+    pool.query(queryString)
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        })
+});
 
 module.exports = router;
